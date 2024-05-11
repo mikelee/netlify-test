e47deadb9e360fac18c2bc562e9512240a8ed5dd
Mike Lee <mike.lee816@gmail.com>
2024-05-11 13:55:45 -0400

    Fix imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
         <h1>ABOUT</h1>
         <Link href='/'>Home</Link>
     </main>
